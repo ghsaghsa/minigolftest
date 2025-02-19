cf8f253fc13ebbe5d818d9b06ab28d58b64b8f86
ghsaghsa <ehsawebsites@gmail.com>
2025-02-19 23:24:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-document.getElementById('cta-button').addEventListener('click', function() {
-    alert('Vielen Dank für Ihr Interesse! Bitte füllen Sie das Kontaktformular aus, um eine Buchung vorzunehmen.');
-});
-
+// Kontaktformular absenden
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Vielen Dank für Ihre Nachricht! Wir werden uns bald bei Ihnen melden.');
